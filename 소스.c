aef536caa74184ae4e7380cbd3c7fb48f326b20e
i5046821854 <77906386+i5046821854@users.noreply.github.com>
2021-02-26 22:27:47 +0900

    브 1100 더하기 사이클

--- 소스.c
+++ 소스.c
@@ -1,43 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "stdio.h"
-#include "string.h"
 
+int main(void) {
+	int num = 0, num1 = 0, num2 = 0;
+	scanf("%d", &num);
+	int i = 1;
+	num1 = num;
+	while (1)
+	{
+		num2 = (num1 % 10) * 10 + ((num1 / 10) + (num1 % 10)) % 10;
+		if (num2 == num)
+			break;
+		else {
+			num1 = num2;
+			i++;
+		}
 
-int main(void)
-{
-	char arr[50][50];
-	int n, m;
-	int num = 0;
-	int a=0, b=0;
-	int i, j;
-	scanf("%d %d", &n, &m);
-	getchar();
-	for (i = 0; i < n; i++)
-	{
-		scanf("%s", arr[i]);
 	}
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < m; j++)
-		{
-			if (arr[i][j] == 'X')
-				break;
-		}
-		if (j == m)
-			a++;
-	}
-	for (j = 0; j < m; j++)
-	{
-		for (i = 0; i < n; i++)
-		{
-			if (arr[i][j] == 'X')
-				break;
-		}
-		if (i == n)
-			b++;
-	}
-	int result = a > b ? a : b;
-	printf("%d", result);
+	printf("%d", i);
 	return 0;
-
 }
